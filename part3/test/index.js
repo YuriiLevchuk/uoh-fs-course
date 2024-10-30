e4fe59527683e9e7b96e51aed2e06b48d85126f2
YuriiLevchuk <yuriilevchuk04@gmail.com>
2024-10-30 11:49:17 +0100

    part3-c / error handling and put/delete requests

--- part3/test/index.js
+++ part3/test/index.js
@@ -1,100 +1,65 @@
-// const http = require('http')
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
-const mongoose = require('mongoose');
+const Note = require('./modules/note')
 
+let notes = [
+]
 
-const app = express();
+app.use(express.static('dist'))
 
-//mongodb defenition
-const url = process.env.MONGODB_URI;
-
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-const Note = mongoose.model('Note', noteSchema);
-
-//middleware
 const requestLogger = (request, response, next) => {
-  console.log('Method: ', request.method);
-  console.log('Path: ', request.path);
-  console.log('Body: ', request.body);
-  console.log('------------');
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
   next()
 }
 
-const unknownEndpoint = (request,response) =>{
-  response.status(404).send({ error:"unknown path" })
+const errorHandler = (error, request, resonse, next) =>{
+  console.error(error);
+
+  if(error.name === 'CastError'){
+    return resonse.status(400).send({ error: 'malformated id' })
+  }
+
+  next(error);
 }
 
-app.use(express.json());
-app.use(express.static('dist'));
-app.use(requestLogger);
-app.use(cors());
+const cors = require('cors')
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+app.use(cors())
 
-// get requests
+app.use(express.json())
+app.use(requestLogger)
+
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response)=>{
+app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes);
+    response.json(notes)
   })
 })
 
-app.get('/api/notes/:id', (request ,response) => {
-  const id = request.params.id;
-  Note.findById(id).then(res =>{
-    response.json(res);
-  })
-})
-
-// delete requests
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter(note => note.id !== id);
-
-  response.status(204).end();
-})
-
-//post requests
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if(!body.content){
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
   const note = new Note({
     content: body.content,
-    important: body.important || false
+    important: body.important || false,
   })
 
   note.save().then(savedNote => {
@@ -102,9 +67,44 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.use(unknownEndpoint);
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error));
+})
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, ()=>{
-  console.log(`server running on port: ${PORT}`);
+app.delete('/api/notes/:id', (request, response) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then( el=>{
+      response.status(204).end();
+    })
+    .catch( error=>next(error) )
+})
+
+app.put('/api/notes/:id', (request, response, next)=>{
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new:true })
+    .then( el=>{
+      response.json(el);
+    })
+    .catch( err=>next(err) );
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
