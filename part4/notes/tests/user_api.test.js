af6743a1e08a2e2f4f4e6b63a6f7131ef981f54c
YuriiLevchuk <yuriilevchuk04@gmail.com>
2024-11-05 13:20:51 +0100

    part4-c / require username creating user

--- part4/notes/tests/user_api.test.js
+++ part4/notes/tests/user_api.test.js
@@ -1,5 +1,13 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const helper = require('./test_helper')
+
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
 
 //...
 
@@ -33,5 +41,26 @@ describe('when there is initially one user in db', () => {
 
     const usernames = usersAtEnd.map(u => u.username)
     assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
   })
 })
